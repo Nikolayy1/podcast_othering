ec7c6150dba9caa36170e55d6a988aecf0c0386a
Niklas Hofstetter <hofiniklas@gmail.com>
2025-12-04 01:03:48 -0700

    adding slurm script

--- train_run.sh
+++ train_run.sh
@@ -7,14 +7,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:h100:1   
-#SBATCH --mem=4G
+#SBATCH --gres=gpu:h100_3g.40gb:1
+#SBATCH --mem=32G
 #SBATCH --time=24:00:00
 #SBATCH --output=slurm_%j.out
 
 #SBATCH --mail-user=niklas.hofstetter@colorado.edu
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=get_data
+#SBATCH --job-name=train_roberta
 
 source ~/.bashrc
 
